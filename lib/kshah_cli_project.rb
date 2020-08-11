39b96fde5baa4571f47c3c81a006fc1d9ff65d0b
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-11 22:36:48 +0000

    initial setup

---
+++ lib/kshah_cli_project.rb
@@ -0,0 +1,6 @@
+require "kshah_cli_project/version"
+
+module KshahCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kshah_cli_project/version.rb
@@ -0,0 +1,3 @@
+module KshahCliProject
+  VERSION = "0.1.0"
+end
