c025db2bf49b5d0245325808a226fd6c56ac4b7c
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-18 20:27:42 +0000

    boilerplate and files

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,7 @@
 require_relative "../lib/kshah_cli_project/version"
 require_relative "../lib/kshah_cli_project/cli"
+require_relative "../lib/kshah_cli_project/get_requester"
+require_relative "../lib/kshah_cli_project/lipstick"
 
 module KshahCliProject
   class Error < StandardError; end
---
+++ lib/kshah_cli_project/lipstick.rb
@@ -0,0 +1,52 @@
+
+class Lipstick
+  
+  attr_accessor :tag_list, :brand, :name, :price, :description, :product_colors
+    @@all = []
+  
+    def initialize(:tag_list)
+      
+      @tag_list = tag_list
+      @brand  = brand
+      @name = name
+      @price = price
+      @descripstion = descripstion
+      @product_colors = product_colors
+      
+      @@all << self 
+    
+    end
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
