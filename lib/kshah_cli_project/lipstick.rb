6655016d301d094aa024cca6338d998fc8f5bee5
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-20 01:34:37 +0000

    added psuedo-code to lipstick class page

--- lib/kshah_cli_project/lipstick.rb
+++ lib/kshah_cli_project/lipstick.rb
@@ -10,5 +10,23 @@ class Lipstick
     def self.all 
       @@all
     end
+  def save
+    @@all << self
+  end
   
+   def self.create(name)
+    song = new(name) 
+    song.save 
+    song
+  end
+  
+  # def self.find_by_name(name) :tag_list = ,  
+  #   all.find {|s| s.name == name}
+  # end 
+  
+  # def self.find_or_create_by_name(name)
+  #   find_by_name(name) || create(name)
+  # end
+  
+ 
 end
