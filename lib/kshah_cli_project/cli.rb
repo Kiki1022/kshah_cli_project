17bb0961a755b97fe4a11aba01e92a7705e2b480
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-23 20:45:18 +0000

    added method to cli class that will convert string to propertag (Capitalize)

--- lib/kshah_cli_project/cli.rb
+++ lib/kshah_cli_project/cli.rb
@@ -9,14 +9,14 @@ class KshahCliProject::Cli
   def menu
     API.get_response_body
     input = ''
-    while input != 'exit' 
+    while input != 'Exit' 
     puts "*For vegan selection, enter 'Vegan'."
     puts "*For all natural selection, enter 'Natural'."
     puts "*For peanut free product selection, enter 'Peanut Free Product'."
     puts "*For gluten free selection, enter 'Gluten Free'."
     puts "*If you have no preferences, enter 'Surprise Me'." 
-    puts "*To leave, type 'exit'."
-    input = gets.strip
+    puts "*To leave, type 'Exit'."
+    input = gets.strip.split(" ").collect {|x| x.capitalize}.join(" ")
     if input == "Surprise Me" 
       Lipstick.all.sample.print_info
     else
