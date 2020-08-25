ff1622488d3f0ee48ada75fbd6e00633c01ed99e
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-25 16:04:29 +0000

    edited indentations for API class

--- lib/kshah_cli_project/api.rb
+++ lib/kshah_cli_project/api.rb
@@ -5,20 +5,19 @@ require 'json'
 
 class KshahCliProject::API
   
-      def self.get_response_body
-        
-        url = "http://makeup-api.herokuapp.com/api/v1/products.json?product_category=lipstick&product_type=lipstick"
-        uri = URI.parse(url)
-        response = Net::HTTP.get_response(uri)
-        lipstick = JSON.parse(response.body) 
-        lipstick.each do |attribute|
-          lip = Lipstick.new 
+  def self.get_response_body
+    url = "http://makeup-api.herokuapp.com/api/v1/products.json?product_category=lipstick&product_type=lipstick"
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    lipstick = JSON.parse(response.body) 
+    lipstick.each do |attribute|
+      lip = Lipstick.new 
           lip.brand = attribute["brand"]
           lip.name = attribute["name"]
           lip.product_link = attribute["product_link"]
           lip.tag_list = attribute["tag_list"]
-        end
-      end
+    end
+  end
 end
  
 
