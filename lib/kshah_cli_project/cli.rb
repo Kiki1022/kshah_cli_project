7e20aea8efaf6c233b621f3dc0ffb3aa5760710b
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-25 22:31:13 +0000

    changed order of options in the CLI class and tidied it up a bit

--- lib/kshah_cli_project/cli.rb
+++ lib/kshah_cli_project/cli.rb
@@ -5,7 +5,7 @@ class KshahCliProject::Cli
     sleep 1
     puts "*****The Lipstick Reader Welcomes You*****".colorize(:light_red)
     sleep 1
-    puts "Loading Menu...beauty takes time".colorize(:light_red)
+    puts "Loading Menu...beauty takes time!".colorize(:light_red)
     KshahCliProject::API.get_response_body
     menu
   end
@@ -15,20 +15,20 @@ class KshahCliProject::Cli
     while input != 'Exit' 
     puts "*To search by brand, enter 'brand'.".colorize(:light_cyan)
     puts "*To have the Lipstick Reader select one for you, enter 'surprise me'.".colorize(:light_cyan)
-    puts "*For a list of brands, enter 'brand'."
-    puts "*For a vegan selection, enter 'vegan'."
-    puts "*For an all natural selection, enter 'natural'."
-    puts "*For a peanut free product selection, enter 'peanut free product'."
-    puts "*For a gluten free selection, enter 'gluten free'."
-    puts "*For an organic selection, enter 'organic'."
-    puts "*For a chemical free selection, enter 'chemical free'."
+    puts "*For a list of brands, enter 'brand'.".colorize(:light_magenta)
+    puts "*For a vegan selection, enter 'vegan'.".colorize(:light_magenta)
+    puts "*For an all natural selection, enter 'natural'.".colorize(:light_magenta)
+    puts "*For a peanut free product selection, enter 'peanut free product'.".colorize(:light_magenta)
+    puts "*For a gluten free selection, enter 'gluten free'.".colorize(:light_magenta)
+    puts "*For an organic selection, enter 'organic'.".colorize(:light_magenta)
+    puts "*For a chemical free selection, enter 'chemical free'.".colorize(:light_magenta)
     puts "~~~To exit, enter 'exit'.~~~".colorize(:light_yellow)
     input = gets.strip.split(" ").collect {|x| x.capitalize}.join(" ")
       if input == "Surprise Me" 
         Lipstick.all.sample.print_info
       elsif input == 'Exit'
         puts "*****Thank you for visiting the Lipstick Reader*****".colorize(:light_red)
-        elsif input == 'Brand'
+      elsif input == 'Brand'
         Lipstick.get_all_brands.each_with_index {|b,i| puts "#{i+1}. #{b}"}
         puts "*Enter the number of the brand for more information:".colorize(:light_cyan)
         number = gets.chomp.to_i 
