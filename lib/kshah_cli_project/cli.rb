eb646e91983ef3b8229ea6cf3b6f1c2911890af0
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-25 22:24:17 +0000

    quick save b/c ide is acting up

--- lib/kshah_cli_project/cli.rb
+++ lib/kshah_cli_project/cli.rb
@@ -5,7 +5,7 @@ class KshahCliProject::Cli
     sleep 1
     puts "*****The Lipstick Reader Welcomes You*****".colorize(:light_red)
     sleep 1
-    puts "Loading Menu...".colorize(:light_red)
+    puts "Loading Menu...beauty takes time".colorize(:light_red)
     KshahCliProject::API.get_response_body
     menu
   end
@@ -13,14 +13,15 @@ class KshahCliProject::Cli
   def menu
     input = ''
     while input != 'Exit' 
+    puts "*To search by brand, enter 'brand'.".colorize(:light_cyan)
+    puts "*To have the Lipstick Reader select one for you, enter 'surprise me'.".colorize(:light_cyan)
     puts "*For a list of brands, enter 'brand'."
-    puts "*For vegan selection, enter 'vegan'."
-    puts "*For all natural selection, enter 'natural'."
-    puts "*For peanut free product selection, enter 'peanut free product'."
-    puts "*For gluten free selection, enter 'gluten free'."
-    puts "*For organic selection, enter 'organic'."
-    puts "*For chemical free selection, enter 'chemical free'."
-    puts "*If you have no preferences, enter 'surprise me'.".colorize(:light_cyan)
+    puts "*For a vegan selection, enter 'vegan'."
+    puts "*For an all natural selection, enter 'natural'."
+    puts "*For a peanut free product selection, enter 'peanut free product'."
+    puts "*For a gluten free selection, enter 'gluten free'."
+    puts "*For an organic selection, enter 'organic'."
+    puts "*For a chemical free selection, enter 'chemical free'."
     puts "~~~To exit, enter 'exit'.~~~".colorize(:light_yellow)
     input = gets.strip.split(" ").collect {|x| x.capitalize}.join(" ")
       if input == "Surprise Me" 
@@ -29,7 +30,7 @@ class KshahCliProject::Cli
         puts "*****Thank you for visiting the Lipstick Reader*****".colorize(:light_red)
         elsif input == 'Brand'
         Lipstick.get_all_brands.each_with_index {|b,i| puts "#{i+1}. #{b}"}
-        puts "Enter the number of the brand for more information:".colorize(:light_cyan)
+        puts "*Enter the number of the brand for more information:".colorize(:light_cyan)
         number = gets.chomp.to_i 
         Lipstick.search_brands(number)
       else
